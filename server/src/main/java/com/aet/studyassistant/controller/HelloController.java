07bac2dcce0b5bbfee1e6b4447537743207e9eab
Yuyang Cao <ge35zoh@mytum.de>
2025-05-14 21:03:52 +0200

    springboot set up with hello service

---
+++ server/src/main/java/com/aet/studyassistant/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.aet.studyassistant.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:3000")
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from Spring Boot!";
+    }
+}
