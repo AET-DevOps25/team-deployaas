8f9593ac9b6eb7a4dee40a6df3b98115a49953ba
Skalski <marcel.skalski@adesso.de>
2025-05-17 19:21:25 +0200

    feat(auth-service): ✨ add and replace auth-service with the server monolith

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,15 @@
 import React, { useEffect, useState } from "react";
 
 function App() {
-  const [message, setMessage] = useState("");
+  const [authMessage, setAuthMessage] = useState("");
   const [quizMessage, setQuizMessage] = useState("");
   const [flashcardMessage, setFlashcardMessage] = useState("");
 
   useEffect(() => {
-    fetch("http://localhost:8080/api/hello")
+    fetch("http://localhost:8080/api/auth/test")
       .then((res) => res.text())
-      .then(setMessage)
-      .catch((err) => console.error("Error fetching:", err));
+      .then(setAuthMessage)
+      .catch((err) => console.error("Error fetching from auth service:", err));
 
     fetch("http://localhost:8081/api/quiz/test")
       .then((res) => res.text())
@@ -26,7 +26,7 @@ function App() {
 
   return (
     <div style={{ padding: "2rem" }}>
-      <h1>{message || "Loading..."}</h1>
+      <h1>{authMessage || "Loading auth service message..."}</h1>
       <h1>{quizMessage || "Loading quiz service message..."}</h1>
       <h1>{flashcardMessage || "Loading flashcard service message..."}</h1>
     </div>
