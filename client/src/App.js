ee35f4cc6adc09ced1512fda6432e8028a5732ac
Skalski <marcel.skalski@adesso.de>
2025-05-17 19:08:20 +0200

    feat(flashcard-service): ✨ add and integrate flashcard-service

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 function App() {
   const [message, setMessage] = useState("");
   const [quizMessage, setQuizMessage] = useState("");
+  const [flashcardMessage, setFlashcardMessage] = useState("");
 
   useEffect(() => {
     fetch("http://localhost:8080/api/hello")
@@ -14,12 +15,20 @@ function App() {
       .then((res) => res.text())
       .then(setQuizMessage)
       .catch((err) => console.error("Error fetching from quiz service:", err));
+
+    fetch("http://localhost:8082/api/flashcard/test")
+      .then((res) => res.text())
+      .then(setFlashcardMessage)
+      .catch((err) =>
+        console.error("Error fetching from flashcard service:", err)
+      );
   }, []);
 
   return (
     <div style={{ padding: "2rem" }}>
       <h1>{message || "Loading..."}</h1>
-      <h2>{quizMessage || "Loading quiz service message..."}</h2>
+      <h1>{quizMessage || "Loading quiz service message..."}</h1>
+      <h1>{flashcardMessage || "Loading flashcard service message..."}</h1>
     </div>
   );
 }
